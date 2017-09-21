fc8410d6d2c4e5ac6762ca9f158b5fb6d0096394
石博文 <sbw@sbw.so>
2017-09-21 21:08:21 +0800

    rewrite with reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 
-extern crate hyper;
-extern crate hyper_tls;
-extern crate num_cpus;
-extern crate regex;
 extern crate clap;
-extern crate tokio_core;
-extern crate futures;
+extern crate regex;
+extern crate reqwest;
 
 use std::io::Read;
 
 use clap::App;
 use clap::Arg;
 use regex::Regex;
-use hyper::{Client, Request, Method, StatusCode};
-use hyper::header::Headers;
-use hyper_tls::HttpsConnector;
-use futures::{Future, Stream};
+use reqwest::Client;
+use reqwest::header::Headers;
 
 macro_rules! read_file {
     ($file:expr) => {{
@@ -58,87 +52,38 @@ fn main() {
     let cookie_file = matches.value_of("cookie").unwrap();
     println!("use cookie file: {}", cookie_file);
 
-    let url = "https://www.v2ex.com/mission/daily";
-    // let url = "https://www.v2ex.com/";
     let cookie = read_file!(cookie_file).trim().to_owned();
-    println!("use cookie: {}", cookie);
-
     let mut headers = Headers::new();
     headers.set_raw("Cookie", vec![cookie.into_bytes()]);
     headers.set_raw("Host", vec![b"www.v2ex.com".to_vec()]);
 
-    let mut core = tokio_core::reactor::Core::new().unwrap();
-    let handle = core.handle();
-    let client = Client::configure()
-        .connector(HttpsConnector::new(num_cpus::get(), &handle).unwrap())
-        .build(&handle);
+    let client = Client::new().unwrap();
+    let url = "https://www.v2ex.com/mission/daily";
+    let mut response = client.get(url).unwrap().headers(headers.clone()).send().unwrap();
+    let mut buf = String::new();
+    response.read_to_string(&mut buf).unwrap();
 
-    let mut req = Request::new(Method::Get, url.parse().unwrap());
-    req.headers_mut().clone_from(&headers);
-    let r = client.request(req).and_then(|r| {
-        r.body().concat2().and_then(|r| {
-            let html = String::from_utf8_lossy(&r[..]);
-            let login_test = Regex::new(r#">登出</a>"#).unwrap();
-            let is_login = login_test.find(&html).is_some();
+    let login_test = Regex::new(r#">登出</a>"#).unwrap();
+    let is_login = login_test.find(&buf).is_some();
 
-            if !is_login {
-                println!("Not Login!!!");
-                return Ok(());
-            }
+    if !is_login {
+        println!("Not Login, exit.");
+        return;
+    }
 
-            let regex = Regex::new(r#"/mission/daily/redeem\?once=\d+"#).unwrap();
-            let caps = match regex.captures(&html) {
-                Some(caps) => caps,
-                _ => {
-                    println!("sign url not found!");
-                    return Ok(());
-                }
-            };
 
-            let url = caps.get(0).unwrap().as_str();
-            let url = format!("https://www.v2ex.com{}", url);
-            println!("{}", url);
+    let regex = Regex::new(r#"/mission/daily/redeem\?once=\d+"#).unwrap();
+    let caps = match regex.captures(&buf) {
+        Some(caps) => caps,
+        _ => {
+            println!("sign url not found!");
+            return;
+        }
+    };
 
-            let mut req = Request::new(Method::Get, url.parse().unwrap());
-            req.headers_mut().clone_from(&headers);
-            let _ = client.request(req).and_then(|r| {
-                if r.status() == StatusCode::Ok {
-                    println!("success");
-                } else {
-                    println!("failed, {:#?}", r);
-                }
+    let url = caps.get(0).unwrap().as_str();
+    let url = format!("https://www.v2ex.com{}", url);
+    println!("{}", url);
 
-                Ok(())
-            });
-
-            Ok(())
-        })
-    });
-
-    let _ = core.run(r).unwrap();
-
-    // let mut response = CLIENT.get(url).headers(headers.clone()).send().unwrap();
-    // let mut buf = String::new();
-    // response.read_to_string(&mut buf).unwrap();
-
-    // let regex = Regex::new(r#"/mission/daily/redeem\?once=\d+"#).unwrap();
-    // let caps = match regex.captures(&buf) {
-    //     Some(caps) => caps,
-    //     _ => {
-    //         println!("sign url not found!");
-    //         return;
-    //     }
-    // };
-
-    // let url = caps.get(0).unwrap().as_str();
-    // let url = format!("https://www.v2ex.com{}", url);
-
-    // let response = CLIENT.get(&url).headers(headers).send().unwrap();
-    // if response.status == StatusCode::Ok {
-    //     println!("success");
-    // } else {
-    //     println!("fail, {:#?}", response);
-    // }
-
-    // save_file!("/tmp/1.txt", buf.as_bytes());
+    client.get(&url).unwrap().headers(headers).send().unwrap();
 }
